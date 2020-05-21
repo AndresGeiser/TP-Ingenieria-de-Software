2acc1e0dfe853214ae637bbe3388a944e7c59b63
AndresGeiser <geiserandres00@gmail.com>
2020-05-21 11:42:07 -0300

    - Lógica de login agregada. - Cambio de fuente en título de login.

--- javascript/formularioLogin.js
+++ javascript/formularioLogin.js
@@ -1,7 +1,35 @@
 
-function saludar()
-{
-    alert("Como vas a iniciar sesión si todavia no esta implementado :P ja ja ja")
+//Toma de datos del formulario
+function login()
+{   
+    var nombreUsuario = document.getElementsByName("nombre");
+    var password = document.getElementsByName("contraseña");
 
+
+    validarDatos(nombreUsuario,password);
+    
 }
+
+//Validación de usuario y contraseña
+function validarDatos(user,pass)
+{
+    var nombreUsuario = user[0].value;
+    var password = pass[0].value;
+
+    if(nombreUsuario == "" || nombreUsuario.length < 4)
+    {
+        alert("Por favor, ingrese un nombre de usuario válido");
+    }
+    else if ( password.length < 8)
+    {
+        alert("La contraseña debe tener más de 8 digitos, vuelva a ingresarla")
+    }
+
+    else
+    {
+        alert("¡Bienvenido " + nombreUsuario + "!" );
+        window.location = "index.html";    
+    }
+}
+
 
