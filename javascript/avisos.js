9fe8c75c21994caf02d49fc58ecc930ab207f30e
FernandezHR <fernandezhr_1998@hotmail.com>
2020-05-23 16:23:10 -0300

    Agregado ventana emergente al seleccionar un aviso

--- javascript/avisos.js
+++ javascript/avisos.js
@@ -1,84 +1,93 @@
 $(document).ready(function() {
     
-    //Obtenemos el boton de filtrar 
-    var boton_filtrar = document.querySelector('.btn_filtrar');
-    
-    //Añadimos al boton la funcion 'filtrar' al hacer click
-    boton_filtrar.addEventListener('click', filtrar);
+    $(".btn_filtrar").click(filtrar);
 
-    function filtrar() {
-        
-        //Obtenemos todos los avisos (obtenemos todos los elementos con clase .card)
-        var avisos = document.querySelectorAll('.card')
+    $(".card").click(mostrarDetalles);
     
-        //Recorremos los avisos y nos fijamos si cumple con los filtros
-        avisos.forEach(aviso => {
-            if(cumpleConFiltros(aviso)) {
-                aviso.style.display = 'inline';
-            } else {
-                aviso.style.display = 'none'; //Ocultamos
-            }
-        });  
-    }
+    $(".cerrar").click(cerrarVentana);
+
     
-    
-    function cumpleConFiltros(aviso) {
-        
-        if(cumpleTipoDeReporte(aviso) && cumpleConEdad(aviso) && cumpleConRaza(aviso)) {
-            return true;
-        }
-        return false;
-    }
-    
-    function cumpleTipoDeReporte(aviso) {
-        if($('.filtro_reporte#adopcion').is(':checked') && aviso.classList.contains('adopcion')) { //.classList devuelve un array con todas las clases de un elemento
-            return true;
-        }
-    
-        if($('.filtro_reporte#perdido').is(':checked') && aviso.classList.contains('perdido')) {
-            return true;
-        }
-    
-        if($('.filtro_reporte#encontrado').is(':checked') && aviso.classList.contains('encontrado')) {
-            return true;
-        }
-        return false;
-    }
-    
-    function cumpleConEdad(aviso) {
-        if($('.filtro_edad#menor_a_uno').is(':checked') && aviso.classList.contains('menor_a_uno')) {
-            return true;
-        }
-    
-        if($('.filtro_edad#uno_a_cinco').is(':checked') && aviso.classList.contains('uno_a_cinco')) {
-            return true;
-        }
-    
-        if($('.filtro_edad#seis_a_diez').is(':checked') && aviso.classList.contains('seis_a_diez')) {
-            return true;
-        }
-    
-        if($('.filtro_edad#mas_de_diez').is(':checked') && aviso.classList.contains('mas_de_diez')) {
-            return true;
-        }
-        return false;
-    }
-    
-    function cumpleConRaza(aviso) {
-    
-        //Obtenemos el elemento selec que contiene las razas
-        var elemento = document.querySelector('.filtro_raza');
-    
-        //Obtenemos el valor de la opcion seleccionada (.options da un arreglo de las opciones del selec, el .selectedIdex da el indice de la opcion que esta seleccionada, el .value nos da el valor de ese atributo)
-        var raza_Seleccionada = elemento.options[elemento.selectedIndex].value;
-    
-        if(raza_Seleccionada == 'todas')  {
-            return true;
-        }           
-        
-        if(aviso.classList.contains(raza_Seleccionada)) {
-            return true;
-        }    
-        return false;
-    }
 });
+
+function filtrar() {
+        
+    //Obtenemos todos los avisos (obtenemos todos los elementos con clase .card)
+    var avisos = document.querySelectorAll('.card')
+
+    //Recorremos los avisos y nos fijamos si cumple con los filtros
+    avisos.forEach(aviso => {
+        if(cumpleConFiltros(aviso)) {
+            aviso.style.display = 'inline';
+        } else {
+            aviso.style.display = 'none'; //Ocultamos
+        }
+    });  
+}
+    
+function cumpleConFiltros(aviso) {
+    
+    if(cumpleTipoDeReporte(aviso) && cumpleConEdad(aviso) && cumpleConRaza(aviso)) {
+        return true;
+    }
+    return false;
+}
+
+function cumpleTipoDeReporte(aviso) {
+    if($('.filtro_reporte#adopcion').is(':checked') && aviso.classList.contains('adopcion')) { //.classList devuelve un array con todas las clases de un elemento
+        return true;
+    }
+
+    if($('.filtro_reporte#perdido').is(':checked') && aviso.classList.contains('perdido')) {
+        return true;
+    }
+
+    if($('.filtro_reporte#encontrado').is(':checked') && aviso.classList.contains('encontrado')) {
+        return true;
+    }
+    return false;
+}
+
+function cumpleConEdad(aviso) {
+    if($('.filtro_edad#menor_a_uno').is(':checked') && aviso.classList.contains('menor_a_uno')) {
+        return true;
+    }
+
+    if($('.filtro_edad#uno_a_cinco').is(':checked') && aviso.classList.contains('uno_a_cinco')) {
+        return true;
+    }
+
+    if($('.filtro_edad#seis_a_diez').is(':checked') && aviso.classList.contains('seis_a_diez')) {
+        return true;
+    }
+
+    if($('.filtro_edad#mas_de_diez').is(':checked') && aviso.classList.contains('mas_de_diez')) {
+        return true;
+    }
+    return false;
+}
+
+function cumpleConRaza(aviso) {
+
+    //Obtenemos el elemento selec que contiene las razas
+    var elemento = document.querySelector('.filtro_raza');
+
+    //Obtenemos el valor de la opcion seleccionada (.options da un arreglo de las opciones del selec, el .selectedIdex da el indice de la opcion que esta seleccionada, el .value nos da el valor de ese atributo)
+    var raza_Seleccionada = elemento.options[elemento.selectedIndex].value;
+
+    if(raza_Seleccionada == 'todas')  {
+        return true;
+    }           
+    
+    if(aviso.classList.contains(raza_Seleccionada)) {
+        return true;
+    }    
+    return false;
+}
+
+function mostrarDetalles() {
+    document.querySelector('.cont_ventana').style.visibility = "visible";
+}
+
+function cerrarVentana() {
+    document.querySelector('.cont_ventana').style.visibility = "hidden";
+}
