ac903b82a8db49890ed851dfa35294555d1c44e1
AndresGeiser <geiserandres00@gmail.com>
2020-05-24 21:31:00 -0300

    -Cambio de tileLayer del mapa.

--- javascript/mapaNegocios.js
+++ javascript/mapaNegocios.js
@@ -2,15 +2,15 @@ function bootstrap()
 {
     let map = L.map('mapid')
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
+    
 
 
-    //Direccion url del geoJson donde se define cada coordenada y sus caracteristicas (en mismo proyecto)
+    //Rescatamos el archivo geoJson del que vamos a obtener los datos
     let geojson_url = "https://raw.githubusercontent.com/AndresGeiser/TP-Ingenieria-de-Software/master/javascript/localizaciones.geojson"
-
     //Tomo los datos de manera asincrónica utilizando promesas y los agrego al mapa
     fetch(
         geojson_url
@@ -18,12 +18,15 @@ function bootstrap()
         res => res.json()
     ).then(
         data => {
-            //Agrego cada dato al mapa, junto a su nombre al hacer click
+
+            //Agrego cada dato al mapa, junto a su nombre y horario al hacer click
             let geoJsonlayer = L.geoJson(data, {
                 onEachFeature: function(feature, layer)
                 {
+                    //Agregamos al popup las propiedades que nos interesan
+                    layer.bindPopup(feature.properties.nombre + 
+                        "<br>" +  feature.properties.horario);
                     
-                    layer.bindPopup(feature.properties.nombre + "<br>" +  feature.properties.horario)
                 }
             }).addTo(map)
             //Centro el mapa sobre los datos
@@ -31,5 +34,6 @@ function bootstrap()
 
         }
     )
+
 
 }
